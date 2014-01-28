132b21950172ee295c5b90fa14bf317bbde97c46
Marco Maggi <marco.maggi-ipsu@poste.it>
2014-01-28 07:42:16 +0100

    fixed admin script to support installation on 64-bit platforms

--- configure.sh
+++ configure.sh
@@ -3,12 +3,17 @@
 set -ex
 
 prefix=/usr/local
+if test -d /lib64
+then libdir=${prefix}/lib64
+else libdir=${prefix}/lib
+fi
 
 ../configure \
     --enable-maintainer-mode                    \
     --config-cache                              \
     --cache-file=../config.cache                \
     --prefix="$prefix"                          \
+    --libdir="$libdir"                          \
     --enable-time-tests                         \
     VFLAGS=-O0					\
     "$@"
