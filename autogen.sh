3ba825167d9d96ad2f45ca133facd0bbba0087de
Marco Maggi <marco.maggi-ipsu@poste.it>
2015-05-29 09:34:25 +0200

    started support for vicare scheme 0.4

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,8 @@
 #
 # Run this in the top source directory to rebuild the infrastructure.
 
-export PATH=/bin:/usr/local/bin:/usr/bin:$PATH
-
 set -xe
-test -d autotools || mkdir autotools
+test -d meta/autotools			|| mkdir meta/autotools
 autoreconf --warnings=all --install --verbose "$@"
 
 ### end of file
